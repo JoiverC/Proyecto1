af18ce15a711540384d1d972e108b609b872754f
Joiver <joiververgara28@gmail.com>
2023-09-21 15:29:20 -0500

    Clase operaciones

---
+++ src/calculadora2/Calculadora2.java
@@ -0,0 +1,20 @@
+
+package calculadora2;
+
+
+
+public class Calculadora2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Interfaz interfaz = new Interfaz();
+        interfaz.setVisible(true);
+        
+        
+        
+    }
+    
+}
---
+++ src/calculadora2/Operaciones.java
@@ -0,0 +1,38 @@
+
+package calculadora2;
+
+
+public class Operaciones {
+    
+    double var1;
+    double var2;
+
+    public Operaciones() {
+        this.var1 = 0;
+        this.var2 = 0;
+    }
+
+    public double getVar1() {
+        return var1;
+    }
+
+    public void setVar1(double var1) {
+        this.var1 = var1;
+    }
+
+    public double getVar2() {
+        return var2;
+    }
+
+    public void setVar2(double var2) {
+        this.var2 = var2;
+    }
+    
+    
+    public int suma(int num1,int num2){
+        int suma = num1 + num2;
+        return suma;
+    }
+    
+    
+}
